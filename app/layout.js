1cb90e9c65f644c378f41dbd69d78d81198727fc
Will <xuy413682@gmail.com>
2026-07-03 16:40:07 -0400

    UI:全站柔和渐变氛围背景+点阵纹理,内部不再是大白底

--- app/layout.js
+++ app/layout.js
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
       <body>
         <I18nProvider>
         <AiErrorProvider>
+          <div className="app-bg" />
           <div className="mx-auto max-w-3xl px-4 pb-28 pt-4 md:pb-10 md:pt-20">{children}</div>
           <Nav />
         </AiErrorProvider>
