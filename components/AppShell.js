e9bea466f87284de883e74d7b65e7e55f4ebc154
Will <xuy413682@gmail.com>
2026-07-05 20:51:54 -0400

    Online questions: generate ORIGINAL practice grounded in real exam format (stop reproducing copyrighted 真题 verbatim); respect no-audio (no listening/map/image questions without media) in the online path too. Hide feedback button on chat page (overlapped send button on mobile)

--- components/AppShell.js
+++ components/AppShell.js
@@ -14,7 +14,7 @@ export default function AppShell({ children }) {
       <div className="app-bg" />
       <div className="relative z-10 mx-auto max-w-3xl px-4 pb-28 pt-4 md:pb-10 md:pt-20">{children}</div>
       <Nav />
-      <FeedbackButton />
+      {path !== "/chat" && <FeedbackButton />}
     </>
   );
 }
